5ea85d4583646167ab734aa36bca8e60a8fe63c6
akegametime <52166668+akegametime@users.noreply.github.com>
2019-07-02 20:50:42 +0700

    Change Money
    #include <iostream>
    
    using namespace std;
    
    int main()
    
    {
    
        short moneyth ;
    	float dollars=30.69;
    	float euro =34.89;
    	cout<<"Input Thai Baths :";
    	cin>>moneyth;
    
    	cout<<moneyth<<"Baths is : "<<moneyth/dollars<<"dollars"<<endl;
    	cout<<moneyth<<"Baths is : "<<moneyth/euro<<"Euro."<<endl;
    
    
    
    	return(0) ;
    
    }

---
+++ ChangeMoney.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+    short moneyth ;
+	float dollars=30.69;
+	float euro =34.89;
+	cout<<"Input Thai Baths :";
+	cin>>moneyth;
+
+	cout<<moneyth<<"Baths is : "<<moneyth/dollars<<"dollars"<<endl;
+	cout<<moneyth<<"Baths is : "<<moneyth/euro<<"Euro."<<endl;
+
+
+
+	return(0) ;
+
+}
